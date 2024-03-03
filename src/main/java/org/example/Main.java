5f16f96cc26311d63663b8dff84674a2725f5c5d
Alex Ov <alexov@swisscows.email>
2024-03-03 20:49:18 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Just in case we need Main hierarchy to demo something
+
+        System.out.println("Hello world!");
+
+    }
+}
